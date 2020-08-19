c6cc825e3c953c963b386637e7a66378cf46c87c
allan <allansaleh95@gmail.com>
2020-08-20 02:41:14 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Acomponent from './components/Acomponent';
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  function handleClick(lang) {
+    i18n.changeLanguage(lang)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => handleClick('en')}>English</button>
+      <button onClick={() => handleClick('de')}>German</button>
+      <Acomponent t={t} />
+    </>
   );
 }
 
---
+++ src/components/Acomponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Acomponent = (props) => {
+
+    return (<p>{props.t('description.part1')}</p>);
+}
+
+export default Acomponent;
