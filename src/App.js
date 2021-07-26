7d36e7f17fe18b763a7dcf5ef5cfcace02984f30
allan <allansaleh95@gmail.com>
2021-07-27 00:45:48 +0300

    removed the react router to show the i18next library functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React from 'react';
-import './App.css';
-import Acomponent from './components/Acomponent';
-import Bcomponent from './components/Bcomponent'
-import { useTranslation } from 'react-i18next'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import React from "react";
+import "./App.css";
+import Acomponent from "./components/Acomponent";
+import Bcomponent from "./components/Bcomponent";
+import { useTranslation } from "react-i18next";
 
 function App() {
-
   const { t, i18n } = useTranslation();
 
   function handleClick(lang) {
-    i18n.changeLanguage(lang)
+    i18n.changeLanguage(lang);
   }
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/B">
-          <Bcomponent />
-        </Route>
-        <Route path="/">
-          <Acomponent />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Bcomponent />
+      <Acomponent />
+    </>
   );
 }
 
